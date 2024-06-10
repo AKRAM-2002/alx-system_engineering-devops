bbebca7ce984341ac5d2856219d4e38eb49bbd3d
akram <akramboutzouga@gmail.com>
2024-06-10 01:04:21 +0100

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,16 +1,19 @@
+# This Puppet manifest fixes the Apache 500 error by ensuring the necessary file permissions and starting the Apache service
+
 # Ensure the necessary file has the correct permissions
-file { '/path/to/file':
+file { '/var/www/html/index.html':
   ensure  => file,
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0644',
 }
 
-# Ensure Apache is installed and running
+# Ensure Apache is installed
 package { 'apache2':
   ensure => installed,
 }
 
+# Ensure Apache service is running
 service { 'apache2':
   ensure  => running,
   enable  => true,
