9147b2e579208002dd83a8966623b6b6a59077ae
AKRAM-2002 <akrambusiness6@gmail.com>
2024-08-15 06:43:03 +0100

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,20 +1,7 @@
 # This Puppet manifest fixes the Apache 500 error by ensuring the necessary file permissions and starting the Apache service
 
-# Ensure the necessary file has the correct permissions
-file { '/var/www/html/index.html':
-  ensure  => file,
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-}
 
-# Ensure Apache is installed
-package { 'apache2':
-  ensure => installed,
-}
-
-# Ensure Apache service is running
-service { 'apache2':
-  ensure  => running,
-  enable  => true,
+exec { 'fix config typo':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
